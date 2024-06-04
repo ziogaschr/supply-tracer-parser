fcf77d80137b6186e04a80eb13130af60cd68cfb
Chris Ziogas <ziogaschr@gmail.com>
2024-06-04 14:44:03 +0300

    modify code for the new supply schema

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/ziogaschr/supply-tracer-parser
 
-go 1.22
+go 1.21
 
 require (
 	github.com/ethereum/go-ethereum v1.13.14
@@ -12,11 +12,15 @@ require (
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fjl/gencodec v0.0.0-20230517082657-f9840df7b83e // indirect
+	github.com/garslo/gogen v0.0.0-20170306192744-1d203ffc1f61 // indirect
 	github.com/holiman/uint256 v1.2.4 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/tools v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- mock_generator.py
+++ mock_generator.py
@@ -1,4 +1,5 @@
 import json
+import random
 import secrets
 
 from Crypto.Hash import keccak
@@ -17,26 +18,38 @@ def random_eth_hash():
     keccak_hash.update(random_bytes)
     return keccak_hash.hexdigest()
 
-def generate_big_integer():
-    # return random.randint(1_000_000_000_000_000_000, 1_000_000_000_000_000_000_000)
-    return 1
+def generate_big_integer_hex():
+    # return hex(random.randint(1_000_000_000_000_000_000, 1_000_000_000_000_000_000_000))
+    return hex(1)
 
 
 with open(file_path, 'w') as file:
     parent_hash = "0x0000000000000000000000000000000000000000000000000000000000000000"
 
-    for block_number in range(1, max_block_number + 1):
+    for block_number in range(0, max_block_number):
         hash = '0x' + random_eth_hash()
 
         data = {
-            "delta": generate_big_integer(),
-            "reward": generate_big_integer(),
-            "withdrawals": generate_big_integer(),
-            "burn": generate_big_integer(),
             "blockNumber": block_number,
             "hash": hash,
             "parentHash": parent_hash
         }
+
+        # genesis block
+        if block_number == 0:
+            data["issuance"] = {
+                "genesisAlloc": generate_big_integer_hex(),
+            }
+        else:
+            data["issuance"] = {
+                "reward": generate_big_integer_hex(),
+                "withdrawals": generate_big_integer_hex(),
+            }
+            data["burn"] = {
+                "eip1559": generate_big_integer_hex(),
+                "blob": generate_big_integer_hex(),
+                "misc": generate_big_integer_hex(),
+            }
 
         file.write(json.dumps(data) + '\n')
 
